21366b24f57d13dac536ccd1b9d75730df6c4d5f
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-05-01 14:47:28 +0300

    add camera follower

---
+++ scorejam18/Assets/_Project/Scripts/CameraFollower.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Gisha.scorejam18
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float followSpeed;
+        [SerializeField] private Vector3 offset;
+
+        private void Update()
+        {
+            Vector3 newPos = target.position + offset;
+            transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
+        }
+    }
+}
