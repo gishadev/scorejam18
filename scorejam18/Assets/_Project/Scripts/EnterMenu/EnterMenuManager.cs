be2bf819e0b9a1b37372de11b414d2848ad2831c
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-05-01 19:34:40 +0300

    create enter scene

---
+++ scorejam18/Assets/_Project/Scripts/EnterMenu/EnterMenuManager.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Gisha.scorejam18.EnterMenu
+{
+    public class EnterMenuManager : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField companyInput;
+        
+        public void OnClick_Enter()
+        {
+            if (!string.IsNullOrEmpty(companyInput.text))
+            {
+                PlayerPrefs.SetString("Nickname", companyInput.text);
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
