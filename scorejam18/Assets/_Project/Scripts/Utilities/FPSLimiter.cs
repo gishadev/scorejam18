a2427caa1273bc96efd5346b1c999aeb0a0fbcf0
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-05-01 17:06:41 +0300

    add fps limiter

---
+++ scorejam18/Assets/_Project/Scripts/Utilities/FPSLimiter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Gisha.scorejam18.Utilities
+{
+    public class FPSLimiter : MonoBehaviour
+    {
+        [SerializeField] private int maxFps = 60;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = maxFps;
+        }
+    }
+}
