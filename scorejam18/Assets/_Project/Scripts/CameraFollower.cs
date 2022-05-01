2021ec869b258f381e3c01be2167d14a473d3185
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-05-01 16:12:12 +0300

    fix camera follow

--- scorejam18/Assets/_Project/Scripts/CameraFollower.cs
+++ scorejam18/Assets/_Project/Scripts/CameraFollower.cs
@@ -8,7 +8,7 @@ namespace Gisha.scorejam18
         [SerializeField] private float followSpeed;
         [SerializeField] private Vector3 offset;
 
-        private void Update()
+        private void FixedUpdate()
         {
             Vector3 newPos = target.position + offset;
             transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
